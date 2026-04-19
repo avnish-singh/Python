aa4e11e0ea17ecb78e1dcac747520c93b8ac7837
Avnish Singh <70936958+avnish-singh@users.noreply.github.com>
2026-04-19 13:50:38 +0530

    Today's Learning

---
+++ conditions.py
@@ -0,0 +1,5 @@
+n = int(input("What's n? "))
+
+if n > 0:     print("n is positive")
+elif n < 0:   print("n is negative")
+else:         print("n is zero")
---
+++ name.py
@@ -0,0 +1,10 @@
+# import sys
+
+# name = input("What is your name? " )
+# print(f"Hello, {name}!")
+
+
+import sys
+
+name = sys.argv[1]
+print(f"Hello, {name}!")
