918cf316d035138a42167bf8a8c92494aa5dbac6
Avnish Singh <70936958+avnish-singh@users.noreply.github.com>
2026-04-22 12:34:14 +0530

    2

--- Week6 File IO/students.py
+++ Week6 File IO/students.py
@@ -12,8 +12,8 @@ with open("students.csv") as file:
         students.append(student)
 
 
-def get_name(student): #sort students by name
-    return student["name"]
+def get_house(student): #sort students by house
+    return student["house"]
 
-for student in sorted(students, key=get_name, reverse=True): #
+for student in sorted(students, key=get_house, reverse=False): #
     print(f"{student['name']} is in {student['house']}")
