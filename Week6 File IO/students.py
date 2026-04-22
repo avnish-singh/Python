5c95ada96b509eb1911ac069f45480b701712c4a
Avnish Singh <70936958+avnish-singh@users.noreply.github.com>
2026-04-22 12:25:25 +0530

    1

--- Week6 File IO/students.py
+++ Week6 File IO/students.py
@@ -1,3 +1,20 @@
+# with open("students.csv") as file:
+#     for line in file:
+#         name, house = line.strip().split(",")
+#         print(f"{name} is in {house}")
+
+students = []
+
 with open("students.csv") as file:
     for line in file:
-        print(line.strip())
+        name, house = line.strip().split(",")
+#         students.append(f"{name} is in {house}")
+
+# for student in sorted(students):
+#     print(student)
+        student = {}
+        student["name"] = name
+        student["house"] = house
+        students.append(student)
+for student in students:
+    print(f"{student['name']} is in {student['house']}")
