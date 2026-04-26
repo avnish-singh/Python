b12ceb91c91d3033170834938bc03612f55f50cb
Avnish Singh <70936958+avnish-singh@users.noreply.github.com>
2026-04-26 15:01:56 +0530

    DictReader

--- Week6 File IO/students.py
+++ Week6 File IO/students.py
@@ -3,17 +3,33 @@
 #         name, house = line.strip().split(",")
 #         print(f"{name} is in {house}")
 
+# students = []
+
+# with open("students.csv") as file:
+#     for line in file:
+#         name, home = line.rstrip().split(",")
+#         student = {"name": name, "home": home}
+#         students.append(student)
+
+
+# for student in sorted(students, key=lambda student: student["name"]): # sort students by name; lamda is an anonymous function that takes a student and returns the student's name
+#     print(f"{student['name']} is from {student['home']}")
+
+
+#CSV Library
+import csv
 students = []
 
-with open("students.csv") as file:
-    for line in file:
-        name, house = line.strip().split(",")
-        student = {"name": name, "house": house}
-        students.append(student)
+# with open('students.csv') as file:
+#     reader = csv.reader(file)
+#     for name, home in reader:
+#         students.append({"name": name, "home": home})
 
 
-def get_house(student): #sort students by house
-    return student["house"]
+with open('students.csv') as file:
+    reader = csv.DictReader(file)
+    for row in reader:
+        students.append({"name": row["name"], "home": row["home"]})
 
-for student in sorted(students, key=get_house, reverse=False): #
-    print(f"{student['name']} is in {student['house']}")
+for student in sorted(students, key=lambda student: student["name"]): # sort students by name; lamda is an anonymous function that takes a student and returns the student's name
+    print(f"{student['name']} is from {student['home']}")
